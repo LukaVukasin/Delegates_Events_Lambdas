135e404d9e29e3df88ed9c244d4de597d1546864
Luka Vukasinovic <luka.vukasinovic@vegait.rs>
2025-05-07 13:55:37 +0200

    Init commit

---
+++ Delegates/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Delegates
+{
+    public delegate int WorkPerformedHandler(int hourse, WorkTypeEnum workType);//Creates a method "type" for function pointers
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //adds the WorkPerformed1 as the WorkPerformedHandler type
+            var delegate1 = new WorkPerformedHandler(WorkPerformed1);
+            var delegate2 = new WorkPerformedHandler(WorkPerformed2);
+
+            //Added the delegate2 to the delegate1, so when delegate1 is called so is delegate2, delegates can be chained together(probably a linked list)
+            delegate1 += delegate2;
+
+            //calls the delegate1, which dups the data to the function called WorkPerformed1, passes or points the data to the WorkPerformed1
+            int finalHours = delegate1(10, WorkTypeEnum.Golf);
+
+            Console.ReadLine();
+        }
+
+        static int WorkPerformed1(int hours, WorkTypeEnum workType)
+        {
+            Console.WriteLine("WorkPerformd1 called " + hours.ToString());
+            return hours + 1;
+        }
+        static int WorkPerformed2(int hours, WorkTypeEnum workType)
+        {
+            Console.WriteLine("WorkPerformd2 called " + workType.ToString());
+            return hours + 1;
+        }
+    }
+
+    public enum WorkTypeEnum
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new Worker();
+            worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(Worker_WorkPerformed);//adding the call back function to the function pipeline
+            worker.WorkCompleted += new EventHandler(Worker_WorkCompleted);
+            //Delegate inference, compiler creates the delegate on its own, simpler syntax
+            worker.WorkCompleted += Worker_WorkCompleted;
+            //This is a lambda function, which is pretty much an annonumous function with a specific signature
+            worker.WorkCompleted += (s, e) => {
+                Console.WriteLine("Work has completed lambda");
+            };
+
+            worker.DoWork(8, WorkTypeEnum.Golf);
+
+            Console.ReadLine();
+        }
+
+        //Call back function/ event handler
+        static void Worker_WorkPerformed(object? sender, WorkPerformedEventArgs e)
+        {
+            Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
+        }
+
+        static void Worker_WorkCompleted(object? sender, EventArgs e)
+        {
+            Console.WriteLine("Work has completed");
+        }
+    }
+
+    public enum WorkTypeEnum
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+
+    //taskkill /IM Events.exe /F command to kill the app if it bugs out
+}
